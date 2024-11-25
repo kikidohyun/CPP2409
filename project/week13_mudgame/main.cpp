e15064703158421f371c1cf5bf7b2c6ec738ec80
kikidohyun <kdh0215tw@naver.com>
2024-11-25 21:21:09 +0900

    Upgrade mudgame

--- project/week13_mudgame/main.cpp
+++ project/week13_mudgame/main.cpp
@@ -8,9 +8,9 @@ const int mapX = 5;
 const int mapY = 5;
 
 // 사용자 정의 함수
-void displayMap(User &user_A,vector<vector<int>> &map,int job);
-void game_excution(User &user_A,vector<vector<int>> &map,int num,int check,int job);
-void out_board(User &user_A, int plus_x,int plus_y);
+void displayMap(User *user_A,vector<vector<int>> &map,int job);
+void game_excution(User *user_A,vector<vector<int>> &map,int num,int job);
+void out_board(User *user_A, int plus_x,int plus_y);
 
 // 메인  함수
 int main() {
@@ -22,7 +22,7 @@ int main() {
 					{3, 0, 0, 0, 2} };
 
 
-	User user_A;
+	User* user_A;
 	int job;
 	// 게임 시작
 	
@@ -41,39 +41,40 @@ int main() {
 		cout<<"잘못 입력하셨습니다."<<endl;
 	}
 	}
+	user_A->DoAttack();
 	while (1)
 	{ // 사용자에게 계속 입력받기 위해 무한 루프
 		string user_input = ""; // 사용자의 입력을 저장할 변수
 
 		
-			cout << "현재  HP는 " << user_A.GetHP() << "입니다." << endl;
+			cout << "현재  HP는 " << user_A->GetHP() << "입니다." << endl;
 			cout << "명령어를 입력하세요 (up,down,left,right,map,end,information): ";
 			cin >> user_input;	
 
 			if (user_input == "up")
 			{
 				// 위로 한 칸 올라가기
-				game_excution(user_A,user_B, map, 8,job);
+				game_excution(user_A, map, 8,job);
 			}
 			else if (user_input == "down")
 			{
 				// TODO: 아래로 한 칸 내려가기
-				game_excution(user_A, user_B,map, 2,job);
+				game_excution(user_A,map, 2,job);
 			}
 			else if (user_input == "left")
 			{
 				// TODO: 왼쪽으로 이동하기
-				game_excution(user_A,user_B, map, 4,job);
+				game_excution(user_A, map, 4,job);
 			}
 			else if (user_input == "right")
 			{
 				// TODO: 오른쪽으로 이동하기
-				game_excution(user_A,user_B, map, 6,job);
+				game_excution(user_A, map, 6,job);
 			}
 			else if (user_input == "map")
 			{
 				// TODO: 지도 보여주기 함수 호출
-				displayMap(user_A, map);
+				displayMap(user_A, map,job);
 			}
 			else if (user_input == "end")
 			{
@@ -82,21 +83,21 @@ int main() {
 			}
 			else if (user_input == "information")
 			{
-				cout << user_A << endl;
+				cout << user_A ;
 			}
 			else
 			{
 				cout << "잘못된 입력입니다." << endl;
 				continue;
 			}
-			if (!user_A.CheckUser(user_A))
+			if (!user_A->CheckUser(user_A))
 			{
 				cout << "실패";
 				break;
 			}
 
 			// 목적지에 도달했는지 체크
-			bool finish = user_A.checkGoal(user_A, map);
+			bool finish = user_A->checkGoal(user_A, map);
 			if (finish == true)
 			{
 				cout << "목적지에 도착했습니다! 축하합니다!" << endl;
@@ -110,16 +111,16 @@ int main() {
 		}
 		
 		
-	
+	delete user_A;
 	return 0;
 }
 
 
 // 지도와 사용자 위치 출력하는 함수
-void displayMap(User &user_A,vector<vector<int>> &map,int job) {
+void displayMap(User *user_A,vector<vector<int>> &map,int job) {
 	for (int i = 0; i < mapY; i++) {
 		for (int j = 0; j < mapX; j++) {
-			if (i == user_A.user_y && j == user_A.user_x) {
+			if (i == user_A->user_y && j == user_A->user_x) {
 				if(job==1){
 					cout<<"     Magician      |";
 				}
@@ -154,13 +155,13 @@ void displayMap(User &user_A,vector<vector<int>> &map,int job) {
 	}
 }
 
-void game_excution(User &user_A, vector<vector<int>> &map, int num,int job)
+void game_excution(User *user_A, vector<vector<int>> &map, int num,int job)
 {
 		if (num == 8)
 		{
-			user_A.user_y -= 1;
-			user_A.DecreaseHP(1);
-			bool inMap = user_A.checkXY(user_A, mapX, mapY);
+			user_A->user_y -= 1;
+			user_A->DecreaseHP(1);
+			bool inMap = user_A->checkXY(user_A, mapX, mapY);
 			if (inMap == false)
 			{
 				out_board(user_A, 0, 1);
@@ -169,14 +170,14 @@ void game_excution(User &user_A, vector<vector<int>> &map, int num,int job)
 			{
 				cout << "위로 한 칸 올라갑니다." << endl;
 				displayMap(user_A, map,job);
-				user_A.checkState(user_A, map);
+				user_A->checkState(user_A, map);
 			}
 		}
 		else if (num == 2)
 		{
-			user_A.user_y += 1;
-			user_A.DecreaseHP(1);
-			bool inMap = user_A.checkXY(user_A, mapX, mapY);
+			user_A->user_y += 1;
+			user_A->DecreaseHP(1);
+			bool inMap = user_A->checkXY(user_A, mapX, mapY);
 			if (inMap == false)
 			{
 				out_board(user_A, 0, -1);
@@ -185,14 +186,14 @@ void game_excution(User &user_A, vector<vector<int>> &map, int num,int job)
 			{
 				cout << "아래로 한 칸 내려갑니다." << endl;
 				displayMap(user_A, map,job);
-				user_A.checkState(user_A, map);
+				user_A->checkState(user_A, map);
 			}
 		}
 		else if (num == 4)
 		{
-			user_A.user_x -= 1;
-			user_A.DecreaseHP(1);
-			bool inMap = user_A.checkXY(user_A, mapX, mapY);
+			user_A->user_x -= 1;
+			user_A->DecreaseHP(1);
+			bool inMap = user_A->checkXY(user_A, mapX, mapY);
 			if (inMap == false)
 			{
 				out_board(user_A, 1, 0);
@@ -201,14 +202,14 @@ void game_excution(User &user_A, vector<vector<int>> &map, int num,int job)
 			{
 				cout << "왼쪽으로 이동합니다." << endl;
 				displayMap(user_A, map,job);
-				user_A.checkState(user_A, map);
+				user_A->checkState(user_A, map);
 			}
 		}
 		else if (num == 6)
 		{
-			user_A.user_x += 1;
-			user_A.DecreaseHP(1);
-			bool inMap = user_A.checkXY(user_A, mapX, mapY);
+			user_A->user_x += 1;
+			user_A->DecreaseHP(1);
+			bool inMap = user_A->checkXY(user_A, mapX, mapY);
 			if (inMap == false)
 			{
 				out_board(user_A, -1, 0);
@@ -217,14 +218,14 @@ void game_excution(User &user_A, vector<vector<int>> &map, int num,int job)
 			{
 				cout << "오른쪽으로 이동합니다." << endl;
 				displayMap(user_A, map,job);
-				user_A.checkState(user_A, map);
+				user_A->checkState(user_A, map);
 			}
 		}
 }
-void out_board(User &user_A, int plus_x,int plus_y){
+void out_board(User *user_A, int plus_x,int plus_y){
 	cout << "맵을 벗어났습니다. 다시 돌아갑니다." << endl;
-	user_A.user_y += plus_y;
-	user_A.user_x += plus_x;
-	user_A.IncreaseHP(1);
+	user_A->user_y += plus_y;
+	user_A->user_x += plus_x;
+	user_A->IncreaseHP(1);
 
 }
--- project/week13_mudgame/user.h
+++ project/week13_mudgame/user.h
@@ -3,51 +3,179 @@
 using namespace std;
 #include <vector>
 
-
 class User
 {
 
-private:
-	int hp;
-	int itemCnt=0;
+protected:
+	int hp=20;
+	int itemCnt = 0;
 
 public:
 	// 유저의 위치를 저장할 변수
-	int user_x=0;
-	int user_y=0;
-    bool checkXY(User &user_A, int mapX, int mapY);
-    bool checkGoal(User &user_A, vector<vector<int>> &map);
-    void checkState(User &user_A, vector<vector<int>> &map);
-	void DecreaseHP(int dec_hp);
-	void IncreaseHP(int inc_hp);
-	int GetHP();
-	User();
-	bool CheckUser(User& user);
-	friend ostream& operator<<(ostream& os, const User& v){
-		
-		os << "현재 HP는 {" << v.hp<< "}이고, 먹은 아이템은 총 {"<<v.itemCnt<<"}개 입니다"<< endl;
+	int user_x = 0;
+	int user_y = 0;
+	User() : hp(20), itemCnt(0) {}
+	virtual bool checkXY(User *user_A, int mapX, int mapY) = 0;
+	virtual bool checkGoal(User *user_A, vector<vector<int>> &map) = 0;
+	virtual void checkState(User *user_A, vector<vector<int>> &map) = 0;
+	virtual void DecreaseHP(int dec_hp) = 0;
+	virtual void IncreaseHP(int inc_hp) = 0;
+	virtual int GetHP() = 0;
+	virtual bool CheckUser(User *user) = 0;
+	friend ostream &operator<<(ostream &os, const User *v)
+	{
+
+		os << "현재 HP는 {" << v->hp << "}이고, 먹은 아이템은 총 {" << v->itemCnt << "}개 입니다" << endl;
 		return os;
 	}
-	virtual void DoAttack()=0;
-
+	virtual void DoAttack() = 0;
 };
 
 class Magician : public User
 {
 public:
+	Magician() {}
 	void DoAttack()
 	{
-		cout<<"공격합니다"<<endl;
+		cout << "공격합니다" << endl;
 		cout << "마법 사용" << endl;
+	}
+	bool checkXY(User *user_A, int mapX, int mapY)
+	{
+		bool checkFlag = false;
+		if (user_A->user_x >= 0 && user_A->user_x < mapX && user_A->user_y >= 0 && user_A->user_y < mapY)
+		{
+			checkFlag = true;
+		}
+		return checkFlag;
+	}
+	// 유저의 위치가 목적지인지 체크하는 함수
+	bool checkGoal(User *user_A, vector<vector<int>> &map)
+	{
+		// 목적지 도착하면
+		if (map[user_A->user_y][user_A->user_x] == 4)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void checkState(User *user_A, vector<vector<int>> &map)
+	{
+		if (map[user_A->user_y][user_A->user_x] == 1)
+		{
+			cout << "{아이템}이 있습니다." << endl;
+			this->itemCnt+=1;
+		}
+		else if (map[user_A->user_y][user_A->user_x] == 2)
+		{
+			cout << "{적}이 있습니다" << endl;
+			user_A->DecreaseHP(2);
+		}
+		else if (map[user_A->user_y][user_A->user_x] == 3)
+		{
+			cout << "{포션}이 있습니다" << endl;
+			user_A->IncreaseHP(2);
+		}
+	}
+	void DecreaseHP(int dec_hp = 1)
+	{
+		this->hp -= dec_hp;
+	}
+	void IncreaseHP(int inc_hp = -1)
+	{
+		this->hp += inc_hp;
+	}
+
+	int GetHP()
+	{
+		return this->hp;
+	}
+
+	bool CheckUser(User *User)
+	{
+		if (this->hp == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 };
 
 class Warrior : public User
 {
 public:
+	Warrior() {}
 	void DoAttack()
 	{
-		cout<<"공격합니다"<<endl;
+		cout << "공격합니다" << endl;
 		cout << "베기 사용" << endl;
+	}
+
+	bool checkXY(User *user_A, int mapX, int mapY)
+	{
+		bool checkFlag = false;
+		if (user_A->user_x >= 0 && user_A->user_x < mapX && user_A->user_y >= 0 && user_A->user_y < mapY)
+		{
+			checkFlag = true;
+		}
+		return checkFlag;
+	}
+	// 유저의 위치가 목적지인지 체크하는 함수
+	bool checkGoal(User *user_A, vector<vector<int>> &map)
+	{
+		// 목적지 도착하면
+		if (map[user_A->user_y][user_A->user_x] == 4)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void checkState(User *user_A, vector<vector<int>> &map)
+	{
+		if (map[user_A->user_y][user_A->user_x] == 1)
+		{
+			cout << "{아이템}이 있습니다." << endl;
+			this->itemCnt+=1;
+		}
+		else if (map[user_A->user_y][user_A->user_x] == 2)
+		{
+			cout << "{적}이 있습니다" << endl;
+			user_A->DecreaseHP(2);
+		}
+		else if (map[user_A->user_y][user_A->user_x] == 3)
+		{
+			cout << "{포션}이 있습니다" << endl;
+			user_A->IncreaseHP(2);
+		}
+	}
+	void DecreaseHP(int dec_hp = 1)
+	{
+		this->hp -= dec_hp;
+	}
+	void IncreaseHP(int inc_hp = -1)
+	{
+		this->hp += inc_hp;
+	}
+
+	int GetHP()
+	{
+		return this->hp;
+	}
+
+	bool CheckUser(User *User)
+	{
+		if (this->hp == 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 };
