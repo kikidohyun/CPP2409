3e504cd6ce94887605c1f64f777816ae3a1bdc84
kikidohyun <kdh0215tw@naver.com>
2024-12-02 20:34:45 +0900

    practice File IO & Exception

---
+++ practice/week14/exception.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pizza_slice=0;
+    int persons =-1;
+    int slice_per_person=0;
+    
+    try
+    {
+        cout<<"피자 조각수를 입력하시오: ";
+        cin >>pizza_slice;
+        cout<<"사람수를 입력하시오: ";
+        cin >> persons;
+        if(persons==0)
+            throw persons;
+        slice_per_person=pizza_slice/persons;
+        cout<<"한사람당 피자는 "<<slice_per_person<<"입니다."<<endl;
+
+    }
+    catch (int e)
+    {
+        cout<<"사람이 "<<e<<"명 입니다."<<endl;
+    }
+    return 0;
+}
